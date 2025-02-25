de30f2a22c61e2bfda3478ea0f61e8e8b2c375e8
Waimea-OBrownrigg <ocbrownrigg@waimea.school.nz>
2025-02-25 13:24:23 +1300

    Learning about lists

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,40 @@
  */
 
 fun main() {
-    println("Hello World!")
+    println("Kotlin MutableList.;")
+
+    val snacks = mutableListOf<String>()
+
+    println(snacks)
+
+    snacks.add("chips (specifically salt and vinegar.)")
+    snacks.add("L&P")
+    snacks.add("dark chocolate")
+
+    println(snacks)
+    println(snacks[0])
+
+    snacks[1] = "sprite"
+
+    println(snacks)
+
+    snacks.removeAt(0)
+    println(snacks)
+
+    snacks.add(0, "Ben approved flavour of chips")
+    println(snacks)
+
+    snacks.sort()
+    println(snacks)
+
+    snacks.shuffle()
+    println(snacks)
+
+    val item = snacks.random()
+    println(item)
+
+    for (snack in snacks) {
+        println(snack)
+    }
 }
 
